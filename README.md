926ec73e401dfb01bc4c16ed156b545dcc0a0825
Marco Aurelio Molinaro <marcomolinario.provider@azulseguros.com.br>
2020-07-30 11:58:39 -0300

    Setting Up ESLint and Prettier

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ From the LinkedIn course
     "Building a Website with Node.js and Express.js" 
 
 - npm install express
+- npm install -D eslint
+    - npx eslint --init
+- npm install -D prettier eslint-config-prettier eslint-plugin-prettier
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express = require("express");
-const { response } = require("express");
+const express = require('express');
 const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, './static')))
+app.use(express.static(path.join(__dirname, './static')));
 
-app.get("/", (request, response) => {
-  response.sendFile(path.join(__dirname, './static/index.html'))
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/index.html'));
 });
 
-app.get("/speakers", (request, response) => {
-  response.sendFile(path.join(__dirname, './static/speakers.html'))
+app.get('/speakers', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, () => {
