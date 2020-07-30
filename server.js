6a813e2bd033255206218c2df13f722babf85537
Marco Aurelio Molinaro <marcomolinario.provider@azulseguros.com.br>
2020-07-30 11:32:33 -0300

    Serving HTML pages and static content

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const { response } = require("express");
+const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, './static')))
+
 app.get("/", (request, response) => {
-  response.send("Hello Express :) ");
+  response.sendFile(path.join(__dirname, './static/index.html'))
+});
+
+app.get("/speakers", (request, response) => {
+  response.sendFile(path.join(__dirname, './static/speakers.html'))
 });
 
 app.listen(port, () => {
