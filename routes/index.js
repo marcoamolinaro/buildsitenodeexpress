f7482530fef0a92a34e0ffde53db68a3a5679566
Marco Aurelio Molinaro <marcomolinario.provider@azulseguros.com.br>
2020-08-13 14:20:28 -0300

    Looping through lists in templates

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,11 @@ const feedbackRoute = require('./feedback');
 const router = express.Router();
 
 module.exports = (params) => {
-  router.get('/', (request, response) => {
-    response.render('layout', { pageTitle: 'Welcome', template: 'index' });
+  const { speakersService } = params;
+
+  router.get('/', async (request, response) => {
+    const topSpeakers = await speakersService.getList();
+    response.render('layout', { pageTitle: 'Welcome', template: 'index', topSpeakers });
   });
 
   router.use('/feedback', feedbackRoute(params));
